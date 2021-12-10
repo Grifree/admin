492f9f58b1ef6148b64e04260c2f3b6ac2141669
nimoc <nimocc@126.com>
2021-12-10 19:09:50 +0800

    Update index.js

--- 2type/module/lbs-limit/index.js
+++ 2type/module/lbs-limit/index.js
@@ -10,11 +10,11 @@ export default {
         area[0].forEach(function (item) {
             let city = []
             if (item.cidx && item.cidx.length === 2) {
-                let cityArea = area[1].slice(item.cidx[0], item.cidx[1])
+                let cityArea = area[1].slice(item.cidx[0], item.cidx[1]+1)
                 cityArea.forEach(function (item) {
                     let district = []
                     if (item.cidx && item.cidx.length === 2) {
-                        let districtArea = area[2].slice(item.cidx[0], item.cidx[1])
+                        let districtArea = area[2].slice(item.cidx[0], item.cidx[1]+1)
                         districtArea.forEach(function (item) {
                             district.push({
                                 label: item.fullname,
